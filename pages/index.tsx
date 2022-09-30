9958f15cb84a8ce887e20336d1d4de4ffa266615
Nick Land <land.d.nicholas@gmail.com>
2022-09-30 18:48:08 -0400

    fix: remove unused page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
+    <section>
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
@@ -13,12 +11,3 @@ const Home: NextPageWithLayout = () => {
 };
 
 export default Home;
-
-Home.getLayout = (page) => {
-  return (
-    <PrimaryLayout>
-      <SidebarLayout />
-      {page}
-    </PrimaryLayout>
-  );
-};
