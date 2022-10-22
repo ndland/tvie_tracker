d25866ada9ec98c8d9996d587fba70cae99c97ac
Nick Land <land.d.nicholas@gmail.com>
2022-10-22 17:32:34 -0400

    fix: add interface for type

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,17 @@ import { useEffect, useState } from 'react';
 import MediaCard from '../components/cards/Media/Media';
 import { NextPageWithLayout } from './page';
 
+/**
+ * @property {string} title - Title of the media
+ * @property {string} poster_path - Path of where the poster resides in TMDB
+ * @property {string} release_date - Date that the media was released
+ */
+export interface Media {
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
+
 const Home: NextPageWithLayout = () => {
   const [trendingMedia, setTrendingMedia] = useState([]);
 
@@ -22,7 +33,7 @@ const Home: NextPageWithLayout = () => {
   return (
     <section>
       <div className="grid grid-cols-5 gap-4 m-4">
-        {trendingMedia.map((media, i) => {
+        {trendingMedia.map((media: Media, i: number) => {
           return (
             <MediaCard
               key={i}
